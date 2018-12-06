6029f01c5a528f326c951bc0d6234073276b791d
xuzhiqian <xzq@mail.ustc.edu.cn>
2018-12-06 10:11:34 +0800

    sift_test

--- database_image.py
+++ database_image.py
@@ -8,6 +8,7 @@ import numpy as np
 conn = sqlite3.connect('datasets.db')
 print("Opened database successfully")
 c = conn.cursor()
+KV = []
 
 # images
 c.execute("INSERT INTO DATASET (ID,NAME,TYPE,ADDRESS) \
@@ -23,18 +24,46 @@ image_root = 'Image/mirflickr1m/images';
 def getRelativePath(path):
     return path[path.find(image_root):]
 
+def abs(x):
+    if x < 0:
+        return -x
+    return x
+
+def dis(a, b):
+    d = 0
+    for i in range(len(a)):
+        d += abs(a[i]-b[i])
+    return d
+
+def nearest(v):
+    min = 99999999999
+    ans = []
+    for s in KV:
+        d = dis(s, v)
+        if d < min:
+            min = d
+            ans = s
+    return ans
+
 def visitPath(path):
     list = os.listdir(path)
     for file in list:
         _path = path + '/' + file
         if os.path.isfile(_path):
-            # kpdes = SIFT(_path)
-            # hashvalue = getImageHashValues(kpdes[1]).tobytes()
-            # c.execute("insert into IMDS values (null, ?, ?, ?)", (file,  getRelativePath(_path), hashvalue))
+            kpdes = SIFT(_path)
+            vectors = kpdes[1]
+            print('Old vector: ', vectors)
+            vectors_new = []
+            for v in vectors:
+                vectors_new.append(nearest(v))
+            print('New vector: ',vectors_new)
+            hashvalue = getImageHashValues(vectors_new).tobytes()
+            c.execute("insert into IMDS values (null, ?, ?, ?)", (file,  getRelativePath(_path), hashvalue))
             c.execute("insert into IMDS values (null, ?, ?, ?)", (file,  getRelativePath(_path), 0))
         else:
             visitPath(_path)
 
+KV = np.load('Image/mirflickr1m/centroid/0-iteration.npy')
 if os.path.exists(image_root):
     visitPath(image_root)
 else:
